2999796b508a243e33b3ca3e38091b3ba4122765
barre <barre@DESKTOP-UK40US8>
2021-03-28 20:45:12 +0200

    first Commit

---
+++ trabajoendes/src/Cuentas/CCuenta.java
@@ -0,0 +1,88 @@
+package Cuentas;
+public class CCuenta {
+
+	/**
+	 * @param cantidad
+	 * @throws Exception
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInter√() {
+		return tipoInter√;
+	}
+
+	public void setTipoInter√(double tipoInter√) {
+		this.tipoInter√ = tipoInter√;
+	}
+/**
+ * creamos varios metodos privados
+ */
+	private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInter√;
+
+    public CCuenta()
+    {
+    }
+/**
+ * 
+ * @param nom
+ * @param cue
+ * @param sal
+ * @param tipo
+ */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    public double estado()
+    {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+/**
+ * 
+ * @param cantidad
+ * @throws Exception
+ */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+}
